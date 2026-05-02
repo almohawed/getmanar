ca44ad242804930f3cc3d56b5bc67c49fbf1fdeb
almohawed <almohawed@github.com>
2026-05-02 19:33:55 +0300

    Update Bundle ID to com.almohawed.manar

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.masar_app"
+    namespace = "com.almohawed.manar"
     compileSdk = 36
     ndkVersion = "27.0.12077973"
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.masar_app"
+        applicationId = "com.almohawed.manar"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
