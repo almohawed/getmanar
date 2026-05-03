e6fde14df120fcc137547f479123cbff8e33728f
almohawed <almohawed@github.com>
2026-05-03 10:35:04 +0300

    fix: add downloadWebTextFile to web_utils stub/web (iOS compile error)

--- lib/src/core/utils/web_utils_stub.dart
+++ lib/src/core/utils/web_utils_stub.dart
@@ -2,3 +2,11 @@
 void evalJavaScript(String code) {
   // No-op on non-web platforms
 }
+
+void downloadWebTextFile(String fileName, String content) {
+  // No-op on non-web platforms
+}
+
+void downloadWebBytesFile(String fileName, List<int> bytes) {
+  // No-op on non-web platforms
+}
--- lib/src/core/utils/web_utils_web.dart
+++ lib/src/core/utils/web_utils_web.dart
@@ -1,7 +1,27 @@
-// Web implementation using dart:js_interop
+// Web implementation using dart:js
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html' as html;
 // ignore: avoid_web_libraries_in_flutter
 import 'dart:js' as js;
 
 void evalJavaScript(String code) {
   js.context.callMethod('eval', [code]);
+}
+
+void downloadWebTextFile(String fileName, String content) {
+  final bytes = html.Blob([content], 'text/plain;charset=utf-8');
+  final url = html.Url.createObjectUrlFromBlob(bytes);
+  final anchor = html.AnchorElement(href: url)
+    ..setAttribute('download', fileName)
+    ..click();
+  html.Url.revokeObjectUrl(url);
+}
+
+void downloadWebBytesFile(String fileName, List<int> bytes) {
+  final blob = html.Blob([bytes]);
+  final url = html.Url.createObjectUrlFromBlob(blob);
+  final anchor = html.AnchorElement(href: url)
+    ..setAttribute('download', fileName)
+    ..click();
+  html.Url.revokeObjectUrl(url);
 }
